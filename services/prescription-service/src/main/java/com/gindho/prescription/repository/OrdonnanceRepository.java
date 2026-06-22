50121ab0ee5ef160fbbff83be5bf50dd884a1faf
ivan-14-dev <ivanbayigabogmis0@gmail.com>
2026-06-22 13:02:27 +0100

    Ajout de Jenkinsfile

--- services/prescription-service/src/main/java/com/gindho/prescription/repository/OrdonnanceRepository.java
+++ services/prescription-service/src/main/java/com/gindho/prescription/repository/OrdonnanceRepository.java
@@ -1,12 +1,12 @@
 package com.gindho.prescription.repository;
-import com.gindho.prescription.model.Ordonnance;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
+
+import com.gindho.prescription.model.Ordonnance;
 public interface OrdonnanceRepository extends JpaRepository<Ordonnance, Long> {
     List<Ordonnance> findByPatientIdOrderByDatePrescriptionDesc(Long patientId);
     Page<Ordonnance> findByPatientIdOrderByDatePrescriptionDesc(Long patientId, Pageable pageable);
-    List<Ordonnance> findByMedecinIdOrderByDatePrescriptionDesc(Long medecinId);
-    Page<Ordonnance> findAll(Pageable pageable);
 }
